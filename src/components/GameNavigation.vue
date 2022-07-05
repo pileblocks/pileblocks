c72b8110336152add8063a1c4651123e878f22ea
Norton <norton1995@protonmail.com>
2022-07-05 23:23:33 +0200

    Changed chevron

--- src/components/GameNavigation.vue
+++ src/components/GameNavigation.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center mt-3">
         <b-button size="sm" variant="secondary" v-if="this.$store.state.Game.gameId > 1" v-on:click="loadPrevGame" class="mr-3"><i class="bi bi-chevron-double-left"></i> {{$t("home.previousGame")}}</b-button>
-        <b-button size="sm" variant="secondary" v-if="this.$store.state.Game.gameId < this.$store.state.Game.lastGameId" v-on:click="loadNextGame"><i class="bi bi-chevron-double-right"></i> {{$t("home.nextGame")}}</b-button>
+        <b-button size="sm" variant="secondary" v-if="this.$store.state.Game.gameId < this.$store.state.Game.lastGameId" v-on:click="loadNextGame">{{$t("home.nextGame")}} <i class="bi bi-chevron-double-right"></i></b-button>
     </div>
 </template>
 
